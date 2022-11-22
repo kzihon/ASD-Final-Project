abd986e0dfbdbb633120dcb00b38879cfe703bac
kidist <KidisGizachew@MacBook-Pro-5.local>
2022-11-21 21:16:40 -0600

    modified

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -5,7 +5,7 @@ import model.IModel;
 import java.util.List;
 
 public interface DAO {
-    public void create(IModel model);
+    public void persist(IModel model);
     public void update(IModel model);
     public IModel get();
     public List<IModel> getAll();
